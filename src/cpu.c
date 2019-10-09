5597100f07455b0a853a6c0d9cf6d3b814800be9
Gabriel Duque <gabriel.duque@procomm-mmc.com>
2019-10-09 20:25:00 +0200

    cpu: set registers to correct initial values

--- src/cpu.c
+++ src/cpu.c
@@ -3,15 +3,16 @@
 #include "gb.h"
 
 /* Initialize CPU to default values */
-void cpu_init(struct gb_s *gb) {
+void cpu_init(struct gb_s *gb)
+{
 
-    /* Set all registers to 0 */
-    gb->cpu.regs.af = 0;
-    gb->cpu.regs.bc = 0;
-    gb->cpu.regs.de = 0;
-    gb->cpu.regs.hl = 0;
-    gb->cpu.regs.sp = 0;
-    gb->cpu.regs.pc = 0;
+    /* Set all registers to their initial value */
+    gb->cpu.regs.af = 0x1b0;
+    gb->cpu.regs.bc = 0x13;
+    gb->cpu.regs.de = 0xd8;
+    gb->cpu.regs.hl = 0x14d;
+    gb->cpu.regs.sp = 0xfffe;
+    gb->cpu.regs.pc = 0x100;
 
     /* Unset all flags */
     gb->cpu.flags.z = false;
