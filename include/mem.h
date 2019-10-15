09869c8c24d412ab2a0e4ee4698b40e1195fe543
Gabriel Duque <gabriel.duque@procomm-mmc.com>
2019-10-15 12:33:35 +0200

    mem: implement fetch functions byte and word

--- include/mem.h
+++ include/mem.h
@@ -1,8 +1,13 @@
 #ifndef BOI_MEM_H
 #define BOI_MEM_H
 
+#include <stdint.h>
+
 #include "gb.h"
 
 void mem_init(struct gb_s *gb);
+
+uint8_t  mem_get_byte(struct gb_s *gb, uint16_t off);
+uint16_t mem_get_word(struct gb_s *gb, uint16_t off);
 
 #endif /* !BOI_MEM_H */
