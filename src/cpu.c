0ec72d8ebb0a6e4eabf6657f07fb0d8f83512062
Gabriel Duque <gabriel.duque@procomm-mmc.com>
2019-10-16 15:18:41 +0200

    ops: implement more opcodes (see diff for details)

--- src/cpu.c
+++ src/cpu.c
@@ -100,7 +100,7 @@ static inline bool func_18(struct gb_s *gb)
 static inline bool func_28(struct gb_s *gb)
 {
     if (gb->cpu.flags.z) {
-        /* TODO: check the +2 here is right */
+        /* TODO: check the +2 here is right skipping operands */
         gb->cpu.regs.pc += (int8_t) mem_get_byte(gb,
                 gb->mem[gb->cpu.regs.sp + 1]) + 2;
         gb->cpu.cycles += 3;
@@ -122,11 +122,42 @@ static inline bool func_3e(struct gb_s *gb)
     return true;
 }
 
+static inline bool func_47(struct gb_s *gb)
+{
+    gb->cpu.regs.b = gb->cpu.regs.a;
+
+    gb->cpu.regs.pc += 1;
+    gb->cpu.cycles += 1;
+
+    return true;
+}
+
 static inline bool func_c3(struct gb_s *gb)
 {
     /* TODO: check we are not out of bounds */
     gb->cpu.regs.pc = mem_get_word(gb, gb->cpu.regs.pc + 1);
     gb->cpu.cycles += 4;
+
+    return true;
+}
+
+static inline bool func_cb(struct gb_s *gb)
+{
+    /* TODO: handle CB prefix */
+    (void) gb;
+    return true;
+}
+
+static inline bool func_cd(struct gb_s *gb)
+{
+    gb->cpu.regs.sp -= 2;
+
+    /* Skip the current instr and it's two byte operand and push next instr */
+    mem_write_word(gb, gb->cpu.regs.sp, gb->cpu.regs.pc + 3);
+
+    gb->cpu.regs.pc = mem_get_byte(gb, gb->cpu.regs.pc + 1);
+
+    gb->cpu.cycles += 6;
 
     return true;
 }
@@ -146,6 +177,17 @@ static inline bool func_af(struct gb_s *gb)
     return true;
 }
 
+static inline bool func_e0(struct gb_s *gb)
+{
+    uint16_t off = 0xff00u + mem_get_byte(gb, gb->cpu.regs.pc + 1);
+    mem_write_byte(gb, off, gb->cpu.regs.a);
+
+    gb->cpu.regs.pc += 2;
+    gb->cpu.cycles += 3;
+
+    return true;
+}
+
 static inline bool func_ea(struct gb_s *gb)
 {
     uint16_t off = mem_get_word(gb, gb->cpu.regs.pc + 1);
@@ -153,6 +195,27 @@ static inline bool func_ea(struct gb_s *gb)
 
     gb->cpu.regs.pc += 3;
     gb->cpu.cycles += 4;
+
+    return true;
+}
+
+static inline bool func_f0(struct gb_s *gb)
+{
+    uint8_t operand = mem_get_byte(gb, gb->cpu.regs.sp + 1);
+    gb->cpu.regs.a = mem_get_byte(gb, 0xff00u + operand);
+
+    gb->cpu.regs.pc += 2;
+    gb->cpu.cycles += 3;
+
+    return true;
+}
+
+static inline bool func_f3(struct gb_s *gb)
+{
+    gb->cpu.irq = false;
+
+    gb->cpu.regs.pc += 1;
+    gb->cpu.cycles += 1;
 
     return true;
 }
@@ -209,33 +272,45 @@ bool cpu_cycle(struct gb_s *gb)
             return func_28(gb);
         case 0x3e: /* ld A, u8 */
             return func_3e(gb);
+        case 0x47: /* ld, B, A */
+            return func_47(gb);
         case 0xaf: /* xor A, A */
             return func_af(gb);
-        case 0xc3: /* 0xc3: jp u16 */
+        case 0xc3: /* jp u16 */
             return func_c3(gb);
-        case 0xd3: /* Invalid */
+        case 0xcb: /* cb prefix */
+            return func_cb(gb);
+        case 0xcd: /* call u16 */
+            return func_cd(gb);
+        case 0xd3: /* invalid */
             return func_invalid(op);
-        case 0xdb: /* Invalid */
+        case 0xdb: /* invalid */
             return func_invalid(op);
-        case 0xdd: /* Invalid */
+        case 0xdd: /* invalid */
             return func_invalid(op);
-        case 0xe3: /* Invalid */
+        case 0xe0: /* ld (0xff00 + u8), A */
+            return func_e0(gb);
+        case 0xe3: /* invalid */
             return func_invalid(op);
-        case 0xe4: /* Invalid */
+        case 0xe4: /* invalid */
             return func_invalid(op);
-        case 0xeb: /* Invalid */
+        case 0xeb: /* invalid */
             return func_invalid(op);
-        case 0xec: /* Invalid */
+        case 0xec: /* invalid */
             return func_invalid(op);
-        case 0xed: /* Invalid */
+        case 0xed: /* invalid */
             return func_invalid(op);
         case 0xea: /* ld (u16), A */
             return func_ea(gb);
-        case 0xf4: /* Invalid */
+        case 0xf0: /* ld A, (0xff00 + u8) */
+            return func_f0(gb);
+        case 0xf3: /* di */
+            return func_f3(gb);
+        case 0xf4: /* invalid */
             return func_invalid(op);
-        case 0xfc: /* Invalid */
+        case 0xfc: /* invalid */
             return func_invalid(op);
-        case 0xfd: /* Invalid */
+        case 0xfd: /* invalid */
             return func_invalid(op);
         case 0xfe: /* cp A, u8 */
             return func_fe(gb);
